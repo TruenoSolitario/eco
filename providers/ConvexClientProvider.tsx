b251bb5e31a2e72ce6233165c83ceb0e734a6e68
Truenosolitario <truenosolitario@gmail.com>
2025-01-06 00:34:17 +0100

    Login with Clerk saving user login info into Convex!

--- providers/ConvexClientProvider.tsx
+++ providers/ConvexClientProvider.tsx
@@ -1,7 +1,8 @@
 "use client"
 
+import LoadingLogo from '@/components/shared/LoadingLogo';
 import { ClerkProvider, useAuth } from '@clerk/nextjs';
-import { ConvexReactClient } from 'convex/react';
+import { Authenticated, AuthLoading, ConvexReactClient } from 'convex/react';
 import { ConvexProviderWithClerk } from 'convex/react-clerk';
 import React from 'react';
 
@@ -16,7 +17,10 @@ const ConvexClientProvider = ({ children }: Props) => {
 	return (
 		<ClerkProvider>
 			<ConvexProviderWithClerk useAuth={useAuth} client={convex}>
-				{children}
+				<Authenticated>{children}</Authenticated>
+				<AuthLoading>
+					<LoadingLogo />
+				</AuthLoading>
 			</ConvexProviderWithClerk>
 		</ClerkProvider>
 	)
